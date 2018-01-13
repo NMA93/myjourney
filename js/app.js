82995e63b8b650f7133c36d19fbd54fe92c86a64
NMA93 <maedernils@hotmail.com>
2018-01-13 16:17:21 +0100

    anpassung

--- js/app.js
+++ js/app.js
@@ -36,5 +36,45 @@ $(window).scroll(function() {
      }
  });
 
-/**Scroll**/
+
+/**Transition**/
+
+$(function(){
+$('.trans-left').onScreen({
+   container: window,
+   direction: 'vertical',
+   doIn: function() {
+   var $el = $(this);
+   $el.addClass('in')
+   },
+   doOut: function() {
+   var $el = $(this);
+   $el.removeClass('in')
+   },
+   tolerance: 0,
+   throttle: 50,
+   toggleClass: 'onScreen',
+   lazyAttr: null,
+   debug: false
+});
+    
+    $('.trans-right').onScreen({
+   container: window,
+   direction: 'vertical',
+   doIn: function() {
+   var $el = $(this);
+   $el.addClass('in')
+   },
+   doOut: function() {
+   var $el = $(this);
+   $el.removeClass('in')
+   },
+   tolerance: 0,
+   throttle: 50,
+   toggleClass: 'onScreen',
+   lazyAttr: null,
+   debug: false
+});
+    });
+    
 
